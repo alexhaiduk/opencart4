0083f7b072b6d777ab250f7f8c551c1d836ca09f
alexhaiduk <alexhaiduk82@gmail.com>
2023-04-11 23:11:57 +0300

    dir design

--- upload/admin/language/ru-ru/design/seo_url.php
+++ upload/admin/language/ru-ru/design/seo_url.php
@@ -11,22 +11,30 @@ $_['text_filter']      = 'Фильтр';
 $_['text_default']     = 'По умолчанию';
 
 // Column
-$_['column_query']     = 'Запрос';
+$_['column_key']           = 'Key';
+$_['column_value']         = 'Value';
 $_['column_keyword']   = 'Ключ';
 $_['column_store']     = 'Магазин';
 $_['column_language']  = 'Язык';
 $_['column_action']    = 'Действие';
 
 // Entry
-$_['entry_query']      = 'Запрос';
-$_['entry_keyword']    = 'Ключ';
 $_['entry_store']      = 'Магазин';
 $_['entry_language']   = 'Язык';
+$_['entry_key']            = 'Key';
+$_['entry_value']          = 'Value';
+$_['entry_keyword']    = 'Ключ';
+$_['entry_sort_order']     = 'Sort Order';
+
+// Help
+$_['help_keyword']         = 'Make sure you only use characters in the a-z or 0-9 use and - or _ for spaces. Leave blank if you just want to remove the perimeter.';
+$_['help_sort_order']      = 'The sort order of the keywords in the URL.';
 
 // Error
 $_['error_permission'] = 'У Вас нет прав для изменения настроек!';
-$_['error_query']      = 'Запрос должен быть от 3 до  64 символов!';
-$_['error_keyword']    = 'Ключ должен быть от 3 до  64 символов!';
 $_['error_exists']     = 'Ключ уже используется!';
-
-
+$_['error_key']            = 'Key must be between 1 and 64 characters!';
+$_['error_value']          = 'Value must be between 1 and 255 characters!';
+$_['error_value_exists']   = 'Value is already in use!';
+$_['error_keyword']    = 'Ключ должен быть от 3 до  64 символов!';
+$_['error_keyword_exists'] = 'Keyword is already in use!';
--- upload/admin/language/ru-ru/design/theme.php
+++ upload/admin/language/ru-ru/design/theme.php
@@ -8,17 +8,16 @@ $_['text_edit']         = 'Редактирование';
 $_['text_store']        = 'Выбрать магазин';
 $_['text_template']     = 'Выбрать шаблон';
 $_['text_default']      = 'По умолчанию';
+$_['text_extension']    = 'Extension';
 $_['text_history']      = 'История';
 $_['text_twig']         = 'Редактор шаблона использует шаблонизатор Twig. Подробнее можете прочитать о <a href="http://twig.sensiolabs.org/documentation" target="_blank" class="alert-link">Twig синтаксисе, тут</a>.';
 
 // Column
 $_['column_store']      = 'Магазин';
 $_['column_route']      = 'Путь';
-$_['column_theme']      = 'Тема';
 $_['column_date_added'] = 'Дата добавления';
 $_['column_action']     = 'Действие';
 
 // Error
 $_['error_permission']  = 'У Вас нет прав для изменения настроек!';
 $_['error_twig']        = 'Внимание: Вы можете сохранить только файл с расширением .twig !';
-
