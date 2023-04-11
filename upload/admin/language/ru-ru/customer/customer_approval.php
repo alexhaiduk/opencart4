3020f3a1c5ef28a2eb0107ae2c38bcefbdf6d1ac
alexhaiduk <alexhaiduk82@gmail.com>
2023-04-11 23:00:06 +0300

    dir customer

--- upload/admin/language/ru-ru/customer/customer_approval.php
+++ upload/admin/language/ru-ru/customer/customer_approval.php
@@ -8,9 +8,12 @@ $_['text_list']             = 'Лист одобрения клиентов';
 $_['text_default']          = 'По умолчанию';
 $_['text_customer']         = 'Клиент';
 $_['text_affiliate']        = 'Партнер';
+$_['text_filter']           = 'Filter';
+$_['text_approve']          = 'Approve';
+$_['text_deny']             = 'Deny';
 
 // Column
-$_['column_name']           = 'Имя';
+$_['column_customer']       = 'Имя';
 $_['column_email']          = 'E-Mail';
 $_['column_customer_group'] = 'Группа';
 $_['column_type']           = 'Тип';
@@ -18,12 +21,12 @@ $_['column_date_added']     = 'Дата добавления';
 $_['column_action']         = 'Действие';
 
 // Entry
-$_['entry_name']            = 'Имя клиента';
+$_['entry_customer']        = 'Имя клиента';
 $_['entry_email']           = 'E-Mail';
 $_['entry_customer_group']  = 'Группа клиента';
 $_['entry_type']            = 'Тип';
-$_['entry_date_added']      = 'Дата добавления';
+$_['entry_date_from']       = 'Date From';
+$_['entry_date_to']         = 'Date To';
 
 // Error
 $_['error_permission']      = 'У Вас нет прав для изменения настроек Одобрение клиентов!';
-
