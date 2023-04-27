05cdbed3aca575a7788bcad3aa9d956b8bef22f0
alexhaiduk <alexhaiduk82@gmail.com>
2023-04-27 23:30:29 +0300

    dir checkout

---
+++ upload/catalog/language/ru-ru/checkout/confirm.php
@@ -0,0 +1,18 @@
+<?php
+// Text
+$_['text_subscription']          = 'Subscription';
+$_['text_subscription_trial']    = 'Стоимость: %s; Периодичность: %d %s; Кол-во платежей: %d;  Далее,  ';
+$_['text_subscription_duration'] = 'Стоимость: %s; Периодичность: %d %s; Кол-во платежей: %d';
+$_['text_subscription_cancel']   = '%s каждые %d %s(й) пока не отменен';
+$_['text_day']                       = 'день';
+$_['text_week']                      = 'неделю';
+$_['text_semi_month']                = 'полмесяца';
+$_['text_month']                     = 'месяц';
+$_['text_year']                      = 'год';
+
+// Column
+$_['column_name']                    = 'Название товара';
+$_['column_model']                   = 'Модель';
+$_['column_quantity']                = 'Количество';
+$_['column_price']                   = 'Цена';
+$_['column_total']                   = 'Итого';
---
+++ upload/catalog/language/ru-ru/checkout/payment_method.php
@@ -0,0 +1,15 @@
+<?php
+// Heading
+$_['heading_title']         = 'Способ оплаты';
+
+// Text
+$_['text_stored']           = 'Выберите способ оплаты';
+$_['text_comments']                  = 'Вы можете добавить комментарий к своему заказу';
+$_['text_agree']                     = 'Я прочитал <a href="%s" class="agree"><b>%s</b></a> и согласен с <br /> условиями безопасности и обработки персональных данных';
+$_['text_success']          = 'Success: You have changed payment method!';
+$_['text_comment']          = 'Success: Comment added!';
+
+// Error
+$_['error_payment_address'] = 'Warning: Payment address required!';
+$_['error_payment_method']  = 'Необходимо указать способ оплаты!';
+$_['error_no_payment']      = 'Warning: No Payment options are available. Please <a href="%s">contact us</a> for assistance!';
